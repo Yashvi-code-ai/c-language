148d262d0424c969886753dc495b9b2b1e31dee9
Yashvi-code-ai <yashvimanaktala66@gmail.com>
2025-09-19 01:31:13 +0530

    Create day5ques2.c

---
+++ day5ques2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+  int totalSeconds,minutes,hours,seconds;
+
+printf("Enter the time in seconds :");
+scanf("%d",&totalSeconds);
+  
+    hours = totalSeconds / 3600;
+    minutes = (totalSeconds % 3600) / 60;
+    seconds = totalSeconds % 60;
+
+    
+    printf("Time in HH:MM:SS format = %02d:%02d:%02d\n", hours, minutes, seconds);
+
+    return 0;
+}
+
+  
