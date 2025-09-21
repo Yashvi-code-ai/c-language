f586ea6d384c6795ed98932180cf21d2e49b0b1e
Yashvi-code-ai <yashvimanaktala66@gmail.com>
2025-09-22 00:51:01 +0530

    Create day17ques2.c

---
+++ day17ques2.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+  int num,i, Prime=1;
+printf("Enter a positive integer: ");
+scanf("%d",&num);
+if (num<=1){
+printf("%d is not a prime number.\n",num);
+ return 0;
+}
+for(i=2;i*i<=num;i++)
+   if (num % i == 0) {
+            Prime = 0;
+            break;
+        }
+    }
+
+    if (Prime)
+        printf("%d is a prime number.\n", num);
+    else
+        printf("%d is not a prime number.\n", num);
+
+    return 0;
+}
+  
