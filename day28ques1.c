a0ee506af9bd2678571fedb05f0e29e046842b7f
Yashvi-code-ai <yashvimanaktala66@gmail.com>
2025-10-16 12:12:29 +0530

    Create day28ques1.c

---
+++ day28ques1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+// Function to check if a number is prime
+bool isPrime(int num) {
+    if(num <= 1) return false;
+    if(num == 2) return true;
+    if(num % 2 == 0) return false;
+
+    for(int i = 3; i * i <= num; i += 2) {
+        if(num % i == 0) return false;
+    }
+    return true;
+}
+
+int main() {
+    int n;
+
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    printf("Prime numbers between 1 and %d are:\n", n);
+
+    for(int i = 2; i <= n; i++) {
+        if(isPrime(i)) {
+            printf("%d ", i);
+        }
+    }
+    printf("\n");
+
+    return 0;
+}
+
