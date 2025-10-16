29bff6c459740045205374dcaa1cc399ffc4b8db
Yashvi-code-ai <yashvimanaktala66@gmail.com>
2025-10-16 12:06:19 +0530

    Create day26ques2.c

---
+++ day26ques2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    // Print first group: 1 star
+    printf("*\n\n");
+
+    // Print second group: 3 stars
+    printf("*\n*\n*\n\n");
+
+    // Print third group: 5 stars
+    printf("*\n*\n*\n*\n*\n\n");
+
+    // Print fourth group: 3 stars
+    printf("*\n*\n*\n\n");
+
+    // Print fifth group: 1 star
+    printf("*\n");
+
+    return 0;
+}
