4080573e4166c5495936409da3466a333ec1a3d2
Yashvi-code-ai <yashvimanaktala66@gmail.com>
2025-10-24 18:48:26 +0530

    Create day43ques2.c

---
+++ day43ques2.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+// Function to check if a string is a palindrome
+int is_palindrome(char str[]) {
+    int i = 0;
+    int j = 0;
+
+    // Find the length of the string
+    while (str[j] != '\0') {
+        j++;
+    }
+    j--; // last character index
+
+    // Compare characters from start and end
+    while (i < j) {
+        if (str[i] != str[j]) {
+            return 0; // Not a palindrome
+        }
+        i++;
+        j--;
+    }
+    return 1; // It's a palindrome
+}
+
+int main() {
+    char str[] = "radar";
+
+    if (is_palindrome(str)) {
+        printf("%s is a palindrome.\n", str);
+    } else {
+        printf("%s is not a palindrome.\n", str);
+    }
+
+    return 0;
+}
